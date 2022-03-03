25c300fad61387ca6932c8c4ba02b7d5c85611c3
Антон <antonL1790@yandex.ru>
2022-03-03 18:56:21 +0300

    Hw 7 Fix

--- src/NewHW7/CollectionNewHW7.java
+++ src/NewHW7/CollectionNewHW7.java
@@ -1,14 +1,15 @@
 package NewHW7;
 
+import java.util.HashSet;
 import java.util.List;
 
 public interface CollectionNewHW7 {
 
     List<Integer> oddNumbers(List<Integer> nums);
 
-    List<Integer> evenUniqueNumbers(List<Integer> nums);
+    HashSet<Integer> evenUniqueNumbers(List<Integer> nums);
 
-    List<String> uniqueWords(List<String> words);
+    HashSet<String> uniqueWords(List<String> words);
 
     int calculationDuplicates(List<String> words);
 }
--- src/NewHW7/CollectionNewHW7Impl.java
+++ src/NewHW7/CollectionNewHW7Impl.java
@@ -1,25 +1,57 @@
 package NewHW7;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class CollectionNewHW7Impl implements CollectionNewHW7 {
+    private boolean isEven(Integer x) {
+        return x % 2 == 0;
+
+    }
+
+    private boolean isOdd(Integer x) {
+        return !isEven(x);
+    }
+
     @Override
     public List<Integer> oddNumbers(List<Integer> nums) {
-        return null;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer num : nums) {
+            if (isOdd(num)) {
+                result.add(num);
+            }
+        }
+        return result;
     }
 
     @Override
-    public List<Integer> evenUniqueNumbers(List<Integer> nums) {
-        return null;
+    public HashSet<Integer> evenUniqueNumbers(List<Integer> nums) {
+        HashSet<Integer> result = new HashSet<>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (isEven(nums.get(i))) {
+                result.add(nums.get(i));
+            }
+        }
+        return result;
     }
 
     @Override
-    public List<String> uniqueWords(List<String> words) {
-        return null;
+    public HashSet<String> uniqueWords(List<String> words) {
+        HashSet<String> result = new HashSet<>();
+        for (int i = 0; i < words.size(); i++) {
+            result.add(words.get(i));
+        }
+        return result;
     }
 
     @Override
     public int calculationDuplicates(List<String> words) {
-        return 0;
+        HashSet<String> result = new HashSet<>();
+        for (int i = 0; i < words.size(); i++) {
+            result.add(words.get(i));
+        }
+        int counterDuplicates = words.size() - result.size();
+        return counterDuplicates;
     }
 }
